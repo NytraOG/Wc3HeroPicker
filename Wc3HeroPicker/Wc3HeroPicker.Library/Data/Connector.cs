33a4068388880285e6c9e6103573a55f1a3d4774
Christian Nytra <christian.nytra@co-orga.de>
2020-09-17 22:42:59 +0200

    Init

---
+++ Wc3HeroPicker/Wc3HeroPicker.Library/Data/Connector.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace Wc3HeroPicker.Library.Data
+{
+    public class Connector
+    {
+        private readonly string connectionString;
+
+        public Connector()
+        {
+            connectionString = ConfigurationManager.ConnectionStrings["Default"].ConnectionString;
+        }
+
+        public 
+    }
+}
---
+++ Wc3HeroPicker/Wc3HeroPicker.Library/Entities/Hero.cs
@@ -0,0 +1,13 @@
+﻿namespace Wc3HeroPicker.Library.Entities
+{
+    public class Hero
+    {
+        public string Id   { get; set; }
+        public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name}";
+        }
+    }
+}
